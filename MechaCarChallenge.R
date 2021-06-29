2545342fbba308b82b184fd6013c6cc2b9e8b394
AntxAmp <78179816+antxamp@users.noreply.github.com>
2021-06-29 11:19:29 -0500

    challenge D3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,3 +8,21 @@ lm_car<-lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearanc
 
 summary(lm_car)
 
+
+#Deliverable2
+
+suspension_df <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary <- suspension_df %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+lot_summary <- suspension_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+
+#Deliverable3
+# Perform t-test across all Lots
+t.test(suspension_df$PSI, mu = 1500)
+t.test(subset(suspension_df,Manufacturing_Lot=='Lot1')$PSI, mu = 1500)
+t.test(subset(suspension_df,Manufacturing_Lot=='Lot2')$PSI, mu = 1500)
+t.test(subset(suspension_df,Manufacturing_Lot=='Lot3')$PSI, mu = 1500)
+
+#Deliverable4 (readme report)
